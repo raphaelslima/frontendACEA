ee854470735b1c24cbf0630ec11f84f3666fe3bf
Raphael Lima <raphaeldesousalm@gmal.com>
2023-11-17 09:53:55 -0300

    feat: add responsive form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -10,14 +10,16 @@ const Form = ()=> {
             <h1 className="text-center text-2xl">Adicione um item</h1>
             <form className="mt-4 flex flex-col justify-center items-center gap-4">
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="itemName">Nome do item</Label>
+                    <Label htmlFor="itemName" className="md:text-lg">Nome</Label>
                     <Input type="text" id="itemName" />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                    <Label htmlFor="qtdItem">Quantidade do item</Label>
-                    <Input type="number" id="qtdItem"/>
+                    <Label htmlFor="qtdItem" className="md:text-lg">Quantidade</Label>
+                    <div className="flex flex-row justify-between items-center md:justify-start md:gap-4">
+                        <Input type="number" id="qtdItem" className="w-1/2"/>
+                        <Button className="bg-green-600 md:text-lg hover:scale-110 hover:bg-green-600">Adicionar</Button>
+                    </div>
                 </div>
-                <Button className="bg-green-600 hover:scale-110 hover:bg-green-600">Adicionar</Button>
             </form>
         </div>
     )
